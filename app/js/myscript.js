46fd789eca11b600035b42846bc38dff6d7f4fed
malamehta <malamehta370@gmail.com>
2022-06-07 14:20:04 +0545

    file added

---
+++ app/js/myscript.js
@@ -0,0 +1,23 @@
+var allNotifications=['10% off Sitewide Use code TEN10 + Free Shipping $99','aaaaaaaaaaa aaaaa','bbbbbb bbbbbbb','ccccc ccccccc'];
+i=0;
+function chngNotification(){
+    document.getElementById("notifications").innerHTML=allNotifications[i];
+    if(i<(allNotifications.length-1)){
+        i++;
+    }else{
+        i=0;
+    }
+}
+setInterval(chngNotification,2000);
+
+// ==========hamberg menu============
+const show = () => {
+    document.getElementById('hamberg-menu').style.left = "-45px";
+    document.getElementById('show-btn').style.display = "none";
+    document.getElementById('hide-btn').style.display = "block";
+  }
+  const hide = () => {
+    document.getElementById('hamberg-menu').style.left = "-120%";
+    document.getElementById('show-btn').style.display = "block";
+    document.getElementById('hide-btn').style.display = "none";
+  }
